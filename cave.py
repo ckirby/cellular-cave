84bf2ed075a39409faf64536b438e8eb73f0a113
Chaim Kirby <misthop@gmail.com>
2018-10-30 11:37:41 -0400

    Add option to show all generations

--- cave.py
+++ cave.py
@@ -1,54 +1,68 @@
+class StableCaveException(Exception):
+    pass
+
+
 class Cave(object):
-    def __init__(self, width, height, ratio, generations, *args, **kwargs):
+    def __init__(self, width, height, ratio, generations, history):
         self.width = width
         self.height = height
-        self.generations = generations
-        self.cave = self.make_cave(ratio*10)
-        self.mutate_cave()
+        self.generations = generations+1
+        self.run(self.make_cave(ratio), history)
+
+    def run(self, cave, should_print):
+        for gen in range(1, self.generations):
+            if should_print:
+                self.print_cave(cave, gen)
+            try:
+                cave = self.mutate_cave(cave)
+            except StableCaveException:
+                if not should_print:
+                    self.print_cave(cave)
+                print(f"Cave mutations converged at generation {gen}")
+                break
+
+        if not should_print:
+            self.print_cave(cave)
 
     def make_cave(self, ratio):
         from random import random
         cave = []
-        random_integers = None
         for x in range(self.height):
             row = []
             for y in range(self.width):
-                try:
-                    cell_integer = random_integers.pop()
-                except (IndexError, AttributeError):
-                    random_integers = [int(ri) for ri in str(random()).split('.')[1].split('e')[0]]
-                    cell_integer = random_integers.pop()
-                finally:
-                    row.append(cell_integer < ratio)
+                row.append(random() < ratio)
             cave.append(row)
 
         return cave
 
-    def mutate_cave(self, generation=0):
-        if generation > self.generations:
-            return
-
-        new_cave = list(self.cave)
+    def mutate_cave(self, cave):
+        from copy import deepcopy
+        new_cave = deepcopy(cave)
         for x in range(self.height):
             for y in range(self.width):
-                new_cave[x][y] = self.check_neighbors(x, y) > 4
+                new_cave[x][y] = self.check_neighbors(cave, x, y) > 4
 
-        self.cave = new_cave
-        self.mutate_cave(generation + 1)
+        if new_cave == cave:
+            raise StableCaveException
 
-    def print_cave(self):
+        return new_cave
+
+
+    def print_cave(self, cave, generation = None):
+        if generation is not None:
+            print(f"Generation {generation}")
         for x in range(self.height):
             for y in range(self.width):
-                print("#", end='') if self.cave[x][y] else print(" ", end='')
+                print("#", end='') if cave[x][y] else print(" ", end='')
             print("")
 
-    def check_neighbors(self, x, y):
+    def check_neighbors(self, cave, x, y):
         live_neighbors = 0
         neighbor_range = range(-1, 2)
         for nx in neighbor_range:
             for ny in neighbor_range:
                 try:
-                    live_neighbors += 1*self.cave[x + nx][y + ny]
+                    live_neighbors += 1*cave[x + nx][y + ny]
                 except IndexError:
                     live_neighbors += .5
 
@@ -56,9 +70,7 @@ class Cave(object):
 
 
 def main(**kwargs):
-    c = Cave(**kwargs)
-    c.print_cave()
-
+    Cave(**kwargs)
 
 def parse_arguments():
     import argparse
@@ -66,8 +78,9 @@ def parse_arguments():
     parser = argparse.ArgumentParser(description='Generate a Cave Map via Cellular Automata')
     parser.add_argument('-W', '--width', type=int, default=80, help='Cave Width')
     parser.add_argument('-H', '--height', type=int, default=50, help='Cave Height')
-    parser.add_argument('-R', '--ratio', type=float, default=.5, help='Ratio of initially alive cells')
-    parser.add_argument('-G', '--generations', type=int, default=3, help='Cellullar Generations')
+    parser.add_argument('-R', '--ratio', type=float, default=0.5, help='Ratio of initially alive cells. 0.3-0.6 recommended')
+    parser.add_argument('-G', '--generations', type=int, default=2, help='Mutation Generations')
+    parser.add_argument('--history', type=bool, default=False, help='Print all Generations')
 
     args = parser.parse_args()
     return vars(args)
